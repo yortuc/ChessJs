d6adef482c3f73be2eebfd7c5e633e2567e24a9b
Evren Yortuçboylu <yortucboylu@gmail.com>
2017-10-15 15:41:29 +0300

    url parse

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ import { toStateString } from './services/stateString'
 // 1 char for turn (white starts so the initial turn is 0)
 const emptyStart = "6a8428a6ccccccccxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxbbbbbbbb597317950"
 
-// Forsyth–Edwards Notation
-// const startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-
 let startingState = emptyStart
 
 if(window.location.pathname.length > 1) {
